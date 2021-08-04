8dc82e885d6561ee37b52c5f92f7c96fbe23947f
Pelentan <38386276+Pelentan@users.noreply.github.com>
2021-08-04 15:28:27 -0400

    stuff went missing

---
+++ aws/leaf-base/lib/leaf-base-stack.d.ts
@@ -0,0 +1,4 @@
+import * as cdk from '@aws-cdk/core';
+export declare class LeafBaseStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
+}
---
+++ aws/leaf-base/lib/leaf-base-stack.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LeafBaseStack = void 0;
+const cdk = require("@aws-cdk/core");
+class LeafBaseStack extends cdk.Stack {
+    constructor(scope, id, props) {
+        super(scope, id, props);
+        // The code that defines your stack goes here
+    }
+}
+exports.LeafBaseStack = LeafBaseStack;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGVhZi1iYXNlLXN0YWNrLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibGVhZi1iYXNlLXN0YWNrLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHFDQUFxQztBQUVyQyxNQUFhLGFBQWMsU0FBUSxHQUFHLENBQUMsS0FBSztJQUMxQyxZQUFZLEtBQW9CLEVBQUUsRUFBVSxFQUFFLEtBQXNCO1FBQ2xFLEtBQUssQ0FBQyxLQUFLLEVBQUUsRUFBRSxFQUFFLEtBQUssQ0FBQyxDQUFDO1FBRXhCLDZDQUE2QztJQUMvQyxDQUFDO0NBQ0Y7QUFORCxzQ0FNQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGNkayBmcm9tICdAYXdzLWNkay9jb3JlJztcblxuZXhwb3J0IGNsYXNzIExlYWZCYXNlU3RhY2sgZXh0ZW5kcyBjZGsuU3RhY2sge1xuICBjb25zdHJ1Y3RvcihzY29wZTogY2RrLkNvbnN0cnVjdCwgaWQ6IHN0cmluZywgcHJvcHM/OiBjZGsuU3RhY2tQcm9wcykge1xuICAgIHN1cGVyKHNjb3BlLCBpZCwgcHJvcHMpO1xuXG4gICAgLy8gVGhlIGNvZGUgdGhhdCBkZWZpbmVzIHlvdXIgc3RhY2sgZ29lcyBoZXJlXG4gIH1cbn1cbiJdfQ==
