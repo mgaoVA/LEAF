ed335e4af83f560a5591d82b0164117f65a0a2ab
Pelentan <38386276+Pelentan@users.noreply.github.com>
2021-08-19 15:02:54 -0400

    hold this

--- aws/leaf-ancil/lib/leaf-ancil-stack.ts
+++ aws/leaf-ancil/lib/leaf-ancil-stack.ts
@@ -1,33 +1,28 @@
 // Manual imports
 import * as cdk from '@aws-cdk/core';
-import { Volume, Vpc } from '@aws-cdk/aws-ec2';
+import { Subnet, Volume, Vpc } from '@aws-cdk/aws-ec2';
 import * as efs from '@aws-cdk/aws-efs';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecs_pats from '@aws-cdk/aws-ecs-patterns';
+import * as ssm from '@aws-cdk/aws-ssm';
 
 // Auto imports.
 import { CfnParameter } from '@aws-cdk/core';
 import { Repository } from '@aws-cdk/aws-ecr';
 import { Duration } from '@aws-cdk/aws-iam/node_modules/@aws-cdk/core';
 import { ApplicationLoadBalancer } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { POINT_CONVERSION_HYBRID } from 'constants';
 
 export class LeafAncilStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // let target_vpc = 'leaf-vpc';
 
-    const target_vpc = new CfnParameter(this, 'target-vpc', {
-      type: 'String',
-      description: "This is the vpc that the all items should be created in.",
-      default: 'leaf-vpc'
-    });
+    const uploadBucketName = new CfnParameter(this, "uploadBucketName", {
+      type: "String",
+      description: "The name of the Amazon S3 bucket where uploaded files will be stored."});
+    console.log("buket: " + uploadBucketName.valueAsString);
 
-    const target_env = new CfnParameter(this, 'target-env', {
-      type: 'String',
-      description: "This is the environment being put into: dev, ca, prod.",
-      default: 'dev',
-      allowedValues: ['dev', 'ca', 'prod'],
-    });
+    const target_env = "prod";
 
     // const vpc = Vpc.fromLookup(this, 'external-vpc', {
     //   vpcName: target_vpc.valueAsString,
@@ -37,45 +32,47 @@ export class LeafAncilStack extends cdk.Stack {
       vpcId: 'vpc-01e40fef11877573b'
     })
 
-    const leaf_base_image = ecs.ContainerImage.fromEcrRepository(
-      Repository.fromRepositoryName(this, 'leaf-monolith', 'leaf-monolith')
-    );
-
     const leaf_efs = new efs.FileSystem(this, 'leaf_filesystem', {
-      fileSystemName: 'project-leaf-' + target_env.valueAsString +'-legacy-efs',
+      fileSystemName: 'project-leaf-' + target_env +'-legacy-efs',
       vpc: vpc,
       enableAutomaticBackups: false,
       encrypted: true,
       lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
-      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,      
+      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE, 
     });
 
-    const leaf_cluster = new ecs.Cluster(this, 'leaf_cluster', {
-      vpc: vpc,
-      clusterName: 'project-leaf-' + target_env.valueAsString + '-cluster'
+    leaf_efs.addAccessPoint('app-files', {
+      path: '/leaf-app-files/', 
+      posixUser: {
+        uid: 'www-data', 
+        gid: 'www-data'
+      }
     });
 
-    new ecs_pats.ApplicationLoadBalancedFargateService(this, 'leaf-fargate', {
-      serviceName: 'leaf-mit-efs',
-      cluster: leaf_cluster,
-      cpu: 512,
-      desiredCount: 1,
-      memoryLimitMiB: 512,
-      loadBalancer: ApplicationLoadBalancer.fromLookup(this, 'yubyub', {
-        loadBalancerArn: 'arn:aws:elasticloadbalancing:us-east-1:456456143286:loadbalancer/app/leaf-prod/afbec84a890a572e'
-      }) ,
-      loadBalancerName: 'leaf-' + target_env.valueAsString,
-      taskImageOptions: {
-        image: leaf_base_image,
-        containerPort: 443,        
-      },
-      healthCheckGracePeriod: Duration.seconds(90),
-      maxHealthyPercent: 200,
-      minHealthyPercent: 100,
-      openListener: true,
-      listenerPort: 443
-
+    leaf_efs.addAccessPoint('sql', {
+      path: '/leaf-sql/', 
+      posixUser: {
+        uid: 'www-data', 
+        gid: 'www-data'
+      }
     });
+
+    leaf_efs.addAccessPoint('user-files', {
+      path: '/leaf-user-files/', 
+      posixUser: {
+        uid: 'www-data', 
+        gid: 'www-data'
+      }
+    });
+    
+    // const efs_accessPoints = new efs.AccessPoint(this, 'efs-acc-points', {
+    //   path: '/leaf-app-files',
+    //   posixUser: {
+    //     uid: 'www-data',
+    //     gid: 'www-data',
+    //     permissions: '777'
+    //   }
+    // })
     
   }
 }
