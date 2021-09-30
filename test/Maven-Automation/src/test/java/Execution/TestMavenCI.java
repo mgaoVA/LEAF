c56a11d63ff3531fef119f375d341fac777b2c90
maxrichardjr <max.richard@adaptivestack.com>
2021-09-29 22:34:35 -0500

    Initial Maven-Automation commit
    2021-09-29

---
+++ test/Maven-Automation/src/test/java/Execution/TestMavenCI.java
@@ -0,0 +1,13 @@
+package Execution;
+
+import org.testng.annotations.Test;
+
+public class TestMavenCI {
+
+	@Test(priority=1)
+	public void TestCI() {
+		System.out.println("Method TestCI() executed");
+	}
+	
+	
+}  //class
